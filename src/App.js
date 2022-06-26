37b86339f500e59f7b05ea205e7d5147cd9ee4d3
Ivan-dp <ivan-dp@bk.ru>
2022-06-26 19:51:35 +0300

    start new course from ulbi-tv. created reducer, store and dispatch functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux/es/exports";
 import "./App.css";
-import Likes from "./Likes";
-import store from "./redux/store";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
+
+  console.log(cash);
+
   return (
-    <div className="App">
-      <div className="wrap">
-        <div className="card">
-          <div className="card-image">
-            <img src={require("./sea.jpg")} alt="surfing" />
-            <Likes store={store} />
-          </div>
-        </div>
+    <div className={"app"}>
+      <div style={{ display: "flex" }}>
+        <button>Пополнить счёт</button>
+        <button>Снять со счёта</button>
       </div>
     </div>
   );
